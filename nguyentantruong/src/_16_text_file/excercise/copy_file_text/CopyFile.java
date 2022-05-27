082962f66235fc7ef2cab842c04bf9f3c5ea13a0
trongghanh23 <truongggh1602@gmail.com>
2022-05-27 09:08:05 +0700

    nộp bài

--- nguyentantruong/src/_16_text_file/excercise/copy_file_text/CopyFile.java
+++ nguyentantruong/src/_16_text_file/excercise/copy_file_text/CopyFile.java
@@ -6,31 +6,30 @@ public class CopyFile {
     public static void main(String[] args) {
         File sourceFile = new File("src/_16_text_file/excercise/copy_file_text/Source.csv");
         File targetFile = new File("src/_16_text_file/excercise/copy_file_text/target.csv");
+        String line;
+        int count = 0;
+        try {
 
-        if (!sourceFile.exists()) {
-            System.out.println("sourceFile không tồn tại: ");
+            FileReader fileReader = new FileReader(sourceFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            FileWriter fileWriter = new FileWriter(targetFile);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            while ((line = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+                count += line.length();
 
-        } else {
-            String line;
-            int count = 0;
-            try (FileReader fileReader = new FileReader(sourceFile);
-                 BufferedReader bufferedReader = new BufferedReader(fileReader);
-                 FileWriter fileWriter = new FileWriter(targetFile);
-                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
-                while ((line = bufferedReader.readLine()) != null) {
-                    bufferedWriter.write(line);
-                    bufferedWriter.newLine();
-                    count += line.length();
-
-                }
-                System.out.println("số kí tự trong chuỗi: " + count);
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("Nhập file  soruce không đúng");
             }
+            bufferedReader.close();
+
+            System.out.println("số kí tự trong chuỗi: " + count);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("File  soruce không đúng hoặc không tồn tại: ");
         }
     }
 }
+
 
 
 
